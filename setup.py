f549f99bd16bedf4f75ba9904242fcad0a25cbbd
Eugene <ebagdasaryan@me.com>
2016-12-01 04:24:47 -0500

    fixed flows

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=['tinycloud',],
     license='MIT License',
     long_description=open('README.md').read(),
-    scripts=['bin/tiny-add-node', 'bin/tiny-add-app', 'bin/tiny-delete-app',
+    scripts=['bin/tiny-add-node', 'bin/tiny-add-app', 'bin/tiny-delete-app', 'bin/tiny-draw',
              'bin/tiny-delete-node', 'bin/tiny-add-flow', 'bin/tiny-deploy'],
     install_requires=[
         'ansible',
